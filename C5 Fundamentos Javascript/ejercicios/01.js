4221b3e510fdc296961d02e87accbc8e0a7acb7b
kavendano <karolav.97@hotmail.com>
2024-02-23 15:02:01 -0300

    update tarea

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = null;
+const nuevoString = "karol avendano";
 
 module.exports = nuevoString;
