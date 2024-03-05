6a3ac46aaf9de2becea6819996bcb85ddfc4dce1
kavendano <karolav.97@hotmail.com>
2024-03-05 01:05:32 -0300

    mensaje de commit

--- C5 Fundamentos Javascript/ejercicios/20.js
+++ C5 Fundamentos Javascript/ejercicios/20.js
@@ -3,6 +3,14 @@ function esVocal(letra) {
   // Si el usuario ingresa un string de más de un caracter, debes retornar el mensaje: "Dato incorrecto".
   // Si no es vocal, tambien debe retornar "Dato incorrecto".
   // Tu código:
+
+  if (letra === a) return "es vocal";
+  if (letra === e) return "es vocal";
+  if (letra === i) return "es vocal";
+  if (letra === o) return "es vocal";
+  if (letra === u) return "es vocal";
+  else "Dato incorrecto";
+  
 }
 
 module.exports = esVocal;
